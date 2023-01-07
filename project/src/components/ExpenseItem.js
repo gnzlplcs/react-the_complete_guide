42819d2aa11018425c9153a8e9a71737b18c485c
gnzlplcs <palaciosgm@outlook.com>
2023-01-07 13:29:57 -0500

    :construction: more complex JSX code
    JSX should return only one element

--- project/src/components/ExpenseItem.js
+++ project/src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const ExpenseItem = () => {
   return (
-    <h2>Expense Item</h2>
-  )
-}
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.90</div>
+      </div>
+    </div>
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
