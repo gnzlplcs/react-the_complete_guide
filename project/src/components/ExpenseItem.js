82e66935443bb361eaa1ae196bfd5487448ddb64
gnzlplcs <palaciosgm@outlook.com>
2023-01-09 17:39:15 -0500

    :construction: split components into multiple components
    create ExpenseDate component / add CSS style / lesson 38

--- project/src/components/ExpenseItem.js
+++ project/src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, date, amount }) => {
-  const month = date.toLocaleString("en-US", { month: "long" });
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
-  const year = date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={date}/>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
