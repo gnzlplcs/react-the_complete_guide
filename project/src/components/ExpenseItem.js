3a62e572f8f76069c45385b79fd3e5934c31ad14
gnzlplcs <palaciosgm@outlook.com>
2023-01-09 16:26:02 -0500

    :construction: add JS logic to components
    lesson 37

--- project/src/components/ExpenseItem.js
+++ project/src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, date, amount }) => {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
