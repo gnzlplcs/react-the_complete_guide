3061ce12f18988fd6471944e258e820678c13d16
gnzlplcs <palaciosgm@outlook.com>
2023-01-09 15:54:09 -0500

    :lipstick: add basic CSS styling
    lesson 34

--- project/src/components/ExpenseItem.js
+++ project/src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
-import React from "react";
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
   return (
-    <div>
+    <div className="expense-item">
       <div>March 28th 2021</div>
       <div>
-        <h2>Car Insurance</h2>
-        <div>$294.90</div>
+        <h2 className="expense-item__description">Car Insurance</h2>
+        <div className="expense-item__price">$294.90</div>
       </div>
     </div>
   );
