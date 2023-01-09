c38cd4264967c11d9389ec03f814c822043d5f64
gnzlplcs <palaciosgm@outlook.com>
2023-01-09 16:16:38 -0500

    :construction: pass data via props
    lesson 36

--- project/src/components/ExpenseItem.js
+++ project/src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-
+const ExpenseItem = ({ title, date, amount }) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
