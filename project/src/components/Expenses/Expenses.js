9ccf4c319d656ca6cb8dc66f6935538cdd1adcf9
gnzlplcs <palaciosgm@outlook.com>
2023-01-12 20:21:15 -0500

    :construction: organizing component files
    lesson 42

--- project/src/components/Expenses/Expenses.js
+++ project/src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from '../UI/Card';
 import "./Expenses.css";
 
 const Expenses = ({ expenses }) => {
