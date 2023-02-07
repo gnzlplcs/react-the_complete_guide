2e5d132b2c2813290aa24a2681b49076c4190783
gnzlplcs <palaciosgm@outlook.com>
2023-02-07 13:22:28 -0500

    :construction: listening events / working w/event handlers
    lesson 46

--- project/src/components/Expenses/ExpenseItem.js
+++ project/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, date, amount }) => {
-
+  const handleClick = () => {
+    console.log("Clicked!");
+  };
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date}/>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
+        <button onClick={handleClick}>Click me!</button>
       </div>
     </Card>
   );
