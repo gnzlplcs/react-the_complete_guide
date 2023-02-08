facd819d165aeb9d2d6e60feb3694e9e4538c88d
gnzlplcs <palaciosgm@outlook.com>
2023-02-08 07:10:58 -0500

    :construction: adding form inputs
    create NewExpense and ExpenseForm components

--- project/src/App.js
+++ project/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -31,7 +32,7 @@ function App() {
 
   return (
     <div>
-      <h1>Let's get started!</h1>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
